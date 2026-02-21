e6ec7de1b9aa018a0ca475e366eca6b766858032
ArtemNizhnik <97676192+ArtemNizhnik@users.noreply.github.com>
2026-02-21 13:23:23 +0000

    task2

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,42 +1,16 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 import dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+dotenv.config();
+
+const BASE_URL = process.env.BASE_URL || 'https://api.jsonbin.io';
+
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: process.env.BASE_URL,
-  },
-
-  /* Configure projects for major browsers */
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-  
+    baseURL: BASE_URL,
+    extraHTTPHeaders: {
+      'X-Master-Key': process.env.JSONBIN_API_KEY || '',
+      'Content-Type': 'application/json'
+    }
+  }
 });
-console.log('BASE_URL:', process.env.BASE_URL);
